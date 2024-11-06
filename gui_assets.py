db1c01b29e4f7f90bdb508ab7348ac805dddee69
FyreAxis <xxcomedyskitzxx@gmail.com>
2024-11-06 13:28:54 -0600

    Adds signup, primitive login/signup authentication , and login/signup save when switching modes.

--- gui_assets.py
+++ gui_assets.py
@@ -1,44 +1,43 @@
 import tkinter as tk
+import gui_funcs
 
 #creates and populates a "label"
 #a label cannot be changed
-loginPrompt = tk.Label(text="Please Log In Below",
+lbl_login = tk.Label(text="Please Log In Below",
     foreground = "black", #shorthand is fg
     background = "thistle", #shorthand is bg
     width = 50, #remember, these are "text units" not pixels
     height = 7 
     )
-usernameLabel = tk.Label(text="Username",
-    foreground = "black", #shorthand is fg
-    background = "thistle", #shorthand is bg
-    width = 25, #remember, these are "text units" not pixels
-    height = 7 
-    )
-passwordLabel = tk.Label(text="Password",
-    foreground = "black", #shorthand is fg
-    background = "thistle", #shorthand is bg
-    width = 25, #remember, these are "text units" not pixels
-    height = 7 
-    )
 #entries let Users type
-usernameField = tk.Entry(fg = "black",
+ent_username = tk.Entry(fg = "black",
     bg = "white",
     width = 30)
-passwordField = tk.Entry(fg = "black",
+ent_password = tk.Entry(fg = "black",
     bg = "white",
     width = 30)
-emailField = tk.Entry(fg = "black",
+ent_email = tk.Entry(fg = "black",
     bg = "white",
     width = 30)
 #actually "packs" the label into the window, otherwise it does nothing
 
-loginButton = tk.Button(text= "Log In", #add authentication
+btn_login = tk.Button(text= "Log In", #add authentication
     fg = "black",
     bg = "thistle",
     width = 5,
-    height = 1)
-signupButton = tk.Button(text= "Sign Up", #add into db, pack emailField
+    height = 1,
+    command = gui_funcs.LoginSignup.loginAuthentication)
+
+btn_signup_navigate = tk.Button(text= "Sign Up", #add into db, pack ent_email
     fg = "black",
     bg = "thistle",
     width = 5,
-    height = 1)
+    height = 1,
+    command = gui_funcs.LoginSignup.openSignup)
+
+btn_signup = tk.Button(text= "Sign Up", #add into db, pack ent_email
+    fg = "black",
+    bg = "thistle",
+    width = 5,
+    height = 1,
+    command = gui_funcs.LoginSignup.signupAuthentication)
--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import tkinter as tk
 import gui_assets
 import gui_funcs
 
-root = tk.Tk() #creates window
-
 #creates our initial login
-gui_funcs.startLogin()
+gui_funcs.bootstrap()
 
-root.mainloop() #prevents actions until window closed
 
